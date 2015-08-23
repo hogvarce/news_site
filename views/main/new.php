28005168af3ec42aab3a59c42cbe115b2f978804
hogvarce <hogvarce@gmail.com>
2015-08-24 00:27:43 +0300

    24.08.15

--- views/main/new.php
+++ views/main/new.php
@@ -1,6 +1,10 @@
+<?php
+  use yii\helpers\Html;
+?>
 <div class="row">
   <div class="col-xs-12">
     <h1><?=$new->title_new?></h1>
+    <?=Html::img($new->smallimg_new, ['alt'=>$new->title_new, 'class'=>'pull-right'])?>
     <?=$new->content_new;?>
   </div>
 </div>
--- views/main/shortNew.php
+++ views/main/shortNew.php
@@ -1,7 +1,7 @@
 <?php
   use yii\helpers\Html;
 ?>
-<div class="col-xs-9 new">
+<div class="col-xs-12 col-sm-12 col-md-9 new">
   <h2><?= Html::a($model->title_new, ['/new/'.$model->id_new])?></h2>
   <div class="clearfix"></div>
   <div class="col-xs-3">
@@ -9,7 +9,7 @@
       <?= Html::a(Html::img($model->smallimg_new, ['alt' => $model->title_new, 'width' => '200']), ['/new/'.$model->id_new]) ?>
     <?php endif; ?>
   </div>
-  <div class="col-xs-9">
+  <div class="col-xs-12 col-sm-12 col-md-9">
     <?=$model->data_new?>
     <?=$model->preview_new?>
     <?= Html::a('читать дальше', ['/new/'.$model->id_new], ['class' => 'more']) ?>
